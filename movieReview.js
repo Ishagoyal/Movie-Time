36919fcd0a133b131bed3bd18d9935473af4a20c
Isha Goyal <iamishagoyal@gmail.com>
2017-06-16 14:15:14 +0530

    API changed from omdb to tmdb

--- movieReview.js
+++ movieReview.js
@@ -9,45 +9,42 @@
           getItems();
 		});
 
+		 
           $scope.search = "3 idiots";
 
 		 function getItems(){
 
-			$http({method:'GET',url:"https://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full"})
+			$http({method:'GET',url:"https://api.themoviedb.org/3/search/movie?api_key=9a1750d469929884b1c959126ec22c83&query=" + $scope.search})
 			.then(function (response) {
 				// body...
-				$scope.movies = response.data;
-				$scope.id = $scope.movies.imdbID;
-		        console.log($scope.id);
+				$scope.moviesData = response.data.results;
+				console.log(response.data.results[0]);
+				$scope.id=response.data.results[0].id;
 
-		        $scope.reviews = [];
-		        $scope.review = {};
-
-		        $scope.addReview = function () {
-
-		        	this.review.createdOn = Date.now();
-		        	// body...
-		        	   this.reviews.push({id: $scope.id,
-		        						 userName: $scope.review.userName,		
-		        						 email: $scope.review.email,
-		        						 stars: $scope.review.stars,
-		        						 message: $scope.review.message,
-		        						 createdOn: $scope.review.createdOn
-		        						});
-		        	this.review = {};
-		        };
-
-		        console.log(this.reviews);
-		        
-	    	});
-
-
-			$http({method:'GET',url:"https://www.omdbapi.com/?s=" + $scope.search})
-			.then(function (response) {
-				// body...
-				$scope.relatedMovies = response.data;
 			
-		    });
+
+				$http({method:'GET',url:"https://api.themoviedb.org/3/movie/" + $scope.id + "?api_key=9a1750d469929884b1c959126ec22c83&append_to_response=credits"})
+			    .then(function (response) {
+				// body...
+				$scope.fullData = response.data;
+				console.log($scope.fullData);
+			    
+
+			});
+
+			});
+
+			
+
+			
+
+
+			// $http({method:'GET',url:"https://www.omdbapi.com/?s=" + $scope.search})
+			// .then(function (response) {
+			// 	// body...
+			// 	$scope.relatedMovies = response.data;
+			
+		 //    });
 
 
 		}
@@ -60,6 +57,26 @@
 
 	         
 
+	});
+
+	app.controller('reviewController', function ($scope) {
+		// body...
+		$scope.reviews = [];
+		$scope.review = {};
+
+
+		        $scope.addReview = function () {
+
+		        	$scope.review.createdOn = Date.now();
+		        	// body...
+		        	$scope.reviews.push({
+		        						 userName: $scope.review.userName,		
+		    		        			 stars: $scope.review.stars,
+		    		        			 message: $scope.review.message,
+		        						 email: $scope.review.email,
+		        						});
+		        	$scope.review = {};
+		        };
 	});
 
                
